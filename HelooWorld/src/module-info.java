2b4f537eafbae134bb3b1df81ddfc6ca27899a40
Jaume <jaumegomiciai@gmail.com>
2023-02-23 14:32:04 +0100

    Hello world

---
+++ HelooWorld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jaume
+ *
+ */
+module HelooWorld {
+}
---
+++ helloWorld/src/helloWorld/main.java
@@ -0,0 +1,10 @@
+package helloWorld;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stud
+		System.out.println("Hola Mundo");
+	}
+
+}
