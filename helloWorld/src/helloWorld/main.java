1d4f60b4ea340f3fc686573c8911515cd44717fa
Jaume <jaumegomiciai@gmail.com>
2023-02-24 14:35:02 +0100

    mas prints

--- helloWorld/src/helloWorld/main.java
+++ helloWorld/src/helloWorld/main.java
@@ -5,6 +5,12 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stud
 		System.out.println("Hola Mundo");
+		System.out.println("3*2");
+		System.out.println(3*2);
+		System.out.printf("5*2=%d",5*2).println();
+		
+		int a = 21;
+		System.out.println(a);
 	}
 
 }
